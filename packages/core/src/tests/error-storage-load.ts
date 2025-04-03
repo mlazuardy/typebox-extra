74b8058dd731ed367e4cf57bb93c8010aa21860b
mlazuardy <lazuardymichael@gmail.com>
2025-04-03 14:26:37 +0700

    chore: add load error storage setup

---
+++ packages/core/src/tests/error-storage-load.ts
@@ -0,0 +1,10 @@
+import { ErrorStorage } from "../error-storage";
+import en from "../locales/en.json";
+import id from "../locales/id.json";
+
+export function loadErrorStorage() {
+  ErrorStorage.load({
+    messages: { en, id },
+    defaultLocale: "en",
+  });
+}
