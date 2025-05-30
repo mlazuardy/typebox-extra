f47b498b4ab44bcce06968dac78125a2f850d6bc
mlazuardy <lazuardymichael@gmail.com>
2025-05-30 15:08:09 +0700

    chore: change email_format name to te_email

--- packages/core/src/custom-formats/email-format.ts
+++ packages/core/src/custom-formats/email-format.ts
@@ -1,7 +1,7 @@
 const EMAIL_PATTERN =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
-export const EMAIL_FORMAT = "email";
+export const EMAIL_FORMAT = "te_email";
 
 export const EmailFormat = (value: string) => {
   return EMAIL_PATTERN.test(value);
